c34cb5fcc735eb7a3a90e984f49b8ead8e5113f3
bergdorf <bergdorf@kth.se>
2018-02-26 22:43:35 +0100

    test: added 2nd test for parse_date function increased to 66% cover improvement from 17%

--- jarviscli/tests/test_manual/test_textParser_parse_date.py
+++ jarviscli/tests/test_manual/test_textParser_parse_date.py
@@ -13,10 +13,22 @@ class ParseDateTest(unittest.TestCase):
         #inputed string's year month and day
         input_string = "2017-03-12"
         res = textParser.parse_date(input_string)
-        self.assertEqual(1, res[0]) #1 string argument is given to the function
+        self.assertEqual(1, res[0]) #when split is applied on input string argument
         self.assertEqual(2017, res[1].year)
         self.assertEqual(3, res[1].month)
         self.assertEqual(12, res[1].day)
+
+    def test_parse_date_2(self):
+        #contract: verifies that a datetime object is returned in idx=1 of returned obj  
+        #and that year of that is +1 of current year when input is 'in one year....'
+        current_year = dt.now().date().year
+        current_month = dt.now().date().month
+        current_day = dt.now().date().day
+        input_string = "in one year and zero month and zero day and zero hour"
+        res = textParser.parse_date(input_string)
+        self.assertEqual((current_year+1), res[1].year)
+        self.assertEqual(current_month, res[1].month)
+        self.assertEqual(current_day, res[1].day)
 
 
 if __name__ == '__main__':
