aa57ae104eac5f3868a0dcab8ff670284e5ca2ee
Albin Remnestål <albin.remnestal@gmail.com>
2018-02-27 12:27:27 +0100

    test: add testcase for faulty src language (#10)
    Improves code coverage from 41% to 56%

--- jarviscli/tests/test_auto/test_translate.py
+++ jarviscli/tests/test_auto/test_translate.py
@@ -33,6 +33,32 @@ class TranslateTest(unittest.TestCase):
         else:
             self.fail('There was no matching output after translation. If the format of the output has changed, the regex used in the test may be obsolete.')
 
+    def test_faulty_source_language(self):
+        """ contract:
+            The translate.main function shall return a correct translation from
+            english to swedish, as per the Google-translate standard, after the
+            first attempt to enter source language fails.
+        """
+        # set up query parameters and oracle for assertion
+        params = ['enaligsh', 'english', 'swedish', 'this is a robot']
+        oracle = u'det h\u00e4r \u00e4r en robot'
+
+        # hijack the file descriptors for simulating user input/output
+        sys.stdin = StringIO(''.join(map(lambda p: '%s\n' % p, params)))
+        sys.stdout = output = StringIO()
+
+        translate.main(Jarvis())
+
+        # extract the translation from stdout
+        sys.stdout  = sys.__stdout__
+        pattern = r'\[\w+\] ((?:.+ ?)*)'
+        translation = re.findall(pattern, output.getvalue().split('\n')[11])
+
+        if (translation):
+            self.assertEqual(oracle, translation[0])
+        else:
+            self.fail('There was no matching output after translation. If the format of the output has changed, the regex used in the test may be obsolete.')
+
     def tearDown(self):
         """ return file descriptors to their absolute origin """
         sys.stdin   = sys.__stdin__
